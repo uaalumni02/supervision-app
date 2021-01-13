545896d080a9dc1bc1d818519b0a92fa104e1aee
Demeco <Demeco_Bell@Demecos-MBP.attlocal.net>
2021-01-12 20:36:30 -0500

    update nav

--- src/components/common/Header/Header.js
+++ src/components/common/Header/Header.js
@@ -4,9 +4,11 @@ import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
+// import Typography from "@material-ui/core/Typography";
+// import Button from "@material-ui/core/Button";
+// import IconButton from "@material-ui/core/IconButton";
+// import MenuIcon from "@material-ui/icons/Menu";
+import { Box, Typography, Button, IconButton, Link } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
 
 import Context from "../../../store/context";
@@ -17,7 +19,7 @@ const useStyles = makeStyles((theme) =>
       flexGrow: 1,
     },
     menuButton: {
-      marginRight: theme.spacing(2),
+      marginLeft: theme.spacing(1),
     },
     title: {
       flexGrow: 1,
@@ -36,29 +38,29 @@ const Header = () => {
   };
   const classes = useStyles();
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            {/* <MenuIcon /> */}
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Chris180
-          </Typography>
-          {!globalState.isLoggedIn && <a href="#">Login</a>}
-          {globalState.isLoggedIn && (
-            <a onClick={handleLogout} href="#">
-              Log Out
-            </a>
-          )}
-        </Toolbar>
-      </AppBar>
-    </div>
+    <Box display="flex" bgcolor="grey.200" p={2} alignItems="center">
+      <Typography>CHRIS 180</Typography>
+      <IconButton
+        href="/meeting"
+        edge="start"
+        className={classes.menuButton}
+        color="inherit"
+        aria-label="menu"
+      >
+        <Typography>Supervision</Typography>
+      </IconButton>
+      <Box flexGrow={2} textAlign="right">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+      </Box>
+      {!globalState.isLoggedIn && <a href="#">Login</a>}
+      {globalState.isLoggedIn && (
+        <a onClick={handleLogout} href="#">
+          Log Out
+        </a>
+      )}
+    </Box>
   );
 };
 
