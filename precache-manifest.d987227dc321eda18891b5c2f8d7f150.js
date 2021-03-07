e0995b8edb97537c3dfb5520be803bf52ee8e921
Demeco <Demeco_Bell@Demecos-MBP.attlocal.net>
2021-03-07 18:35:44 -0500

    Updates

---
+++ precache-manifest.d987227dc321eda18891b5c2f8d7f150.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3564e81ec9868d2de6817039010a4121",
+    "url": "/supervision-app/index.html"
+  },
+  {
+    "revision": "4b94fd41f1d49a3bd2c0",
+    "url": "/supervision-app/static/css/2.eff9287b.chunk.css"
+  },
+  {
+    "revision": "19dd656b0f2fc912e64e",
+    "url": "/supervision-app/static/css/main.c06f581a.chunk.css"
+  },
+  {
+    "revision": "4b94fd41f1d49a3bd2c0",
+    "url": "/supervision-app/static/js/2.76dab1f3.chunk.js"
+  },
+  {
+    "revision": "556d72da1c19c1e88e2917fa6505d7f0",
+    "url": "/supervision-app/static/js/2.76dab1f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19dd656b0f2fc912e64e",
+    "url": "/supervision-app/static/js/main.de79b46d.chunk.js"
+  },
+  {
+    "revision": "d9f97ec3d176d51ed870",
+    "url": "/supervision-app/static/js/runtime-main.dc3712ad.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/supervision-app/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/supervision-app/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "3f28b642568815dd3ddd79ad2238be92",
+    "url": "/supervision-app/static/media/avatar.3f28b642.png"
+  }
+]);
